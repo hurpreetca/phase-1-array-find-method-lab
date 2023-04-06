a6c553ef2f2163d49c3e6ee24ee2a361a93e9271
Harpreet Singh <hurpreet@icloud.com>
2023-04-05 20:46:06 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(array)
+{
+    if(array.find(obj => obj.result=== 'W' )){
+        return array.find(obj => obj.result=== 'W').year
+    }
+    else 
+    {
+        return undefined;
+    }
+}
